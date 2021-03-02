e60180859ee0c1ccd4b0667df4674abcf956006a
Tendai Nyandoro <tendai1@hotmail.com>
2021-03-02 16:27:14 +0200

    Add the function to check to the listen to which button has been clicked

---
+++ app.js
@@ -0,0 +1,13 @@
+// set initial count 
+let count = 0;
+
+// Select value and buttons
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function(e){
+    
+  })
+});
